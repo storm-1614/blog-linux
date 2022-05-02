9b3219185f8d66b178a9aa03ef2ff2dececcfa0c
storm-1614 <yejinwei20070430@163.com>
2022-05-02 14:00:43 +0800

    dwm

--- linux/dwm/config.h
+++ linux/dwm/config.h
@@ -6,20 +6,20 @@ static const char *downvol[] = { "amixer", "-qM", "set", "Master", "5%-", "umute
 static const char *upvol[]   = { "amixer", "-qM", "set", "Master", "5%+", "umute", NULL };
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 10;        /* 1 means no outer gap when there is only one window */
+static const int showbar            = 10;        /* 0 means no bar */
+static const int topbar             = 10;        /* 0 means bottom bar */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;   /* 0 means no systray */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "PingFang SC Bold:size=14"};
-static const char dmenufont[]       = "PingFang SC Bold:size=14";
+static const char *fonts[]          = { "Hack NF:size=14", "PingfFang SC:size=14"};
+static const char dmenufont[]       = "Hack NF:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -39,8 +39,9 @@ static const unsigned int alphas[][3]      = {
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 /* tagging */
-static const char *tags[] = { "\uf015¹", "\ue5fe²", "\uf030³", "\uf268⁴", "\uf269⁵", "\ue62a⁶", "\ue217⁷", "\uf2dc⁸", "\uf30d⁹" };
-
+static const char *tags[] = {"\uf120¹", "\ue5fe²", "\uf030³","\uf268⁴", "\uf269⁵", "\ue62a⁶","\ue217⁷", "\uf2dc⁸", "\uf684⁹"};
+/*static const char *tags[] = { "\uf120¹", "\uf7ae²", "\uf121³", "\ue04b⁴", "\uf62e⁵", "\uf251⁶", "\ue727⁷", "\uf537⁸", "\uf684⁹" };*/
+/*static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\ue04b", "\uf62e", "\uf251", "\ue727", "\uf537", "\uf684" };*/
 static const Rule rules[] = {
 	 /* xprop(1):
 	  *	WM_CLASS(STRING) = instance, class
@@ -87,14 +88,16 @@ static const char *chromecmd[]  = { "microsoft-edge-stable",  NULL };
 static const char *radomchwp[]  = { "/home/storm/.config/scripts/random-change-sp.sh",  NULL };
 static const char *screenshotcmd[] = { "deepin-screenshot", NULL };
 static const char *firefoxcmd[]  = { "firefox",  NULL };
+static const char *file[] = {"nautilus", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+  { MODKEY,                       XK_e,      spawn,          {.v = file}}, /*打开gnome文件*/
 	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },/*打开ropfi*/
 	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = roficmd1 } },/*以命令模式打开rofi*/
-	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = screenshotcmd } },/*打开火焰截图*/
-  { MODKEY,                       XK_p,      spawn,          {.v = screenshotcmd } },/*打开火焰截图*/
-	{ MODKEY,                       XK_c,      spawn,          {.v = chromecmd } },/*打开chrome*/
+	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = screenshotcmd } },/*截图*/
+  { MODKEY,                       XK_p,      spawn,          {.v = screenshotcmd } },/*截图*/
+	{ MODKEY,                       XK_c,      spawn,          {.v = chromecmd } },/*打开edge*/
 	{ MODKEY,                       XK_r,      spawn,          {.v = radomchwp } },/*随机切换壁纸*/
 	{ Mod1Mask,                     XK_f,      spawn,          {.v = firefoxcmd } },/*打开firefox*/
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },//打开dmenu
