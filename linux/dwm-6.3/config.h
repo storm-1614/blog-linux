81f68c5c32f039bf0bbf346237bfe6303f218660
风云雾 <storm1614@outlook.com>
2022-08-15 12:14:15 +0800

    dwm

--- linux/dwm-6.3/config.h
+++ linux/dwm-6.3/config.h
@@ -15,6 +15,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Hack NF:size=14", "Source Han Sans CN:size=14" };
+//static const char *fonts[]          = { "Hack NF:size=14", "LXGW WenKai:style=Regular:size=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#2e3440";
