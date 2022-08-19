82b8fc56a397a66241baf493c944137f68a9032f
风云雾 <storm1614@outlook.com>
2022-08-19 09:30:42 +0800

    sh

---
+++ zsh/setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+if [ ! $(command -v apt) ] ;then
+	sleep 0.1
+	else sudo apt install fzf exa zsh bat -y
+fi
+
+if [ ! $(command -v pacman) ] ;then
+	sleep 0.1
+	else sudo pacman -S fzf exa bat zsh --noconfirm 
+fi
+
+echo "正在解压中！"
+7z x ./oh-my-zsh.7z ~/. >/dev/null 2>&1
+echo "正在复制zshrc"
+cp ./zshrc ~/.zshrc
+echo "完成"
+
+
