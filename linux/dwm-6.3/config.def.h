f0547b544dd983992ffb19fd7d1c6194a873eda2
风云雾 <storm1614@outlook.com>
2022-08-21 15:38:01 +0800

    window

--- linux/dwm-6.3/config.def.h
+++ linux/dwm-6.3/config.def.h
@@ -64,8 +64,13 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 7,       1,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Zathura",  NULL, 	  NULL, 	  1 << 1,  		0, 			 -1 },
+	{ "VirtualBox Manager", NULL, NULL,   1 << 5, 		0, 			 -1 },
+	{ "VirtualBox Machine", NULL, NULL,   1 << 5, 		0, 			 -1 },
+	{ "icalingua", NULL,      NULL, 	  1 << 4, 		0, 			 -1 },
+	{ "TelegramDesktop", NULL, NULL,      1 << 3, 		0, 			 -1 },
 };
 
 /* layout(s) */
